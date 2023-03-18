d764b6e77683f0f730857ac10dbdae4ab43974c7
Leonardo Soares Dos Santos <123108396+gitlableo@users.noreply.github.com>
2023-03-17 21:14:58 -0300

    teste

---
+++ Aulas/exercicio exemplo.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+ //fazer comentario somente aqui
+//funcao principal do programa
+int main (){
+
+//definindo variaveis
+int a = 6, b = 8, c = 12;
+
+//realizando a expressao
+printf("\n A multiplicacao de %d e %d subtraido de %d = %d", 6, 8, 12, a * b -c);
+
+return 0;
+}
